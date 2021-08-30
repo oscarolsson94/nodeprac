ddb2358f8d00d3ae480d30ebdb50a8aa13855533
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-30 20:12:29 +0200

    adding to user array

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24,
+  },
+];
+
 /* /users is common routes for all enpoints */
 router.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(users);
 });
 
 export default router;
