87efc666cf9f2465329688d7c40765d2d53b193e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-30 19:09:20 +0200

    first api endpoint

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  console.log("TEST!");
+  res.send("Hello from Homepage");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
