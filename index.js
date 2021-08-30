bc17c197c0a0773b53f6fbff4eacdd12bf6aa334
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-30 20:47:42 +0200

    bodyparser not needed anymore

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/users", usersRoutes); // set default path for all routes
 
