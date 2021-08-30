a627d96908a0168c032f1476aee5c86311199657
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-30 14:28:32 +0200

    global functions in node

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const sayHello = () => console.log("hello");
 /* sayHello(); */
 
 /* console.log(window); // cant reach document or window object in node */
+
+// Global functions in node:
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
